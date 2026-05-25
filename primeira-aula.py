67738d3ce77b692f5992345ea8608bee6e6fd97f
Luciana Busquets <lubusquets@hotmail.com>
2026-05-25 20:42:55 -0300

    Primeira Aula

---
+++ primeira-aula.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome: ")
+print("Olá", nome,"prazer em te conhecer!")
+dia = input("Digite o dia do seu nascimento: ")
+mes = input("Digite o mês do seu nascimento: ")
+ano = input("Digite o ano do seu nascimento: ")
+print("Você nasceu no dia", dia, "do mês", mes, "do ano", ano,".Correto?")
+num = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+soma = num + num2
+print("A soma dos números é:", soma)
+
