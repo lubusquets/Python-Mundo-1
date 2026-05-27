b11e7a028939130129ea76ca502817d8847b2206
Luciana Busquets <lubusquets@hotmail.com>
2026-05-27 19:32:40 -0300

    conversor de moeda

---
+++ conversor-USD.py
@@ -0,0 +1,9 @@
+print('Conversor de Real para Dólar')
+print(40*'*')
+r = float(input('Digite um valor em R$: '))
+
+print('O valor de {:.2f} reais corresponde a {:.2f} dólares'.format(r,r*5.07)) #cotação do dólar em 2024-06-01.
+
+
+
+
