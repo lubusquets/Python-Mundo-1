06d7d5e2398830737b0e0e74fb1bf353c4217330
Luciana Busquets <lubusquets@hotmail.com>
2026-05-27 20:41:47 -0300

    desconto

--- conversor-USD.py
+++ conversor-USD.py
@@ -1,7 +1,7 @@
 print('Conversor de Real para Dólar')
 print(40*'*')
-r = float(input('Digite um valor em R$: '))
 
+r = float(input('Digite um valor em R$: '))
 print('O valor de {:.2f} reais corresponde a {:.2f} dólares'.format(r,r*5.07)) #cotação do dólar em 2024-06-01.
 
 
---
+++ valor-com-desconto.py
@@ -0,0 +1,12 @@
+print('VALOR COM DESCONTO')
+print(40*'-')
+
+v = float(input('Digite o valor do produto: '))
+d = float(v*0.05) #5% de desconto
+
+print('O valor do produto com desconto é R${:.2f}.'.format(v-d))
+
+
+
+      
+      
