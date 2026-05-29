ba3172d0465bca73f65bc62641aba2d6445af76a
Luciana Busquets <lubusquets@hotmail.com>
2026-05-28 21:51:45 -0300

    observações

--- novo-salário.py
+++ novo-salário.py
@@ -2,8 +2,9 @@ print('           NOVO SALÁRIO        ')
 print(30*'*')
 
 n = (input('Digite o nome do funcionário: '))
-s = float(input('Digite o salário atual R$ '))
+s = float(input('Digite o salário atual: R$ '))
 sn = float(s*0.15) #15% de aumento
+#ou sn = s+(s*15/100)
 print('O novo salário de {} é R${:.2f}.'.format(n,s+sn))
 
 
