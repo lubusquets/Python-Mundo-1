402f562cd7da94dad781b6b45d08a973d52b8115
Luciana Busquets <lubusquets@hotmail.com>
2026-06-03 19:01:21 -0300

    sorteio

--- calculo-hipotenusa.py
+++ calculo-hipotenusa.py
@@ -15,3 +15,13 @@ print('O seno de {} é {:.2f}'.format(angulo, seno))
 print('O cosseno de {} é {:.2f}'.format(angulo, cosseno))
 print('A tangente de {} é {:.2f}'.format(angulo, tangente))
 
+print(40*'-')
+#sorteio de nome 
+import random
+n1 = str(input("Digite o nome do primeiro aluno: "))
+n2 = str(input("Digite o nome do segundo aluno: "))             
+n3 = str(input("Digite o nome do terceiro aluno: "))
+n4 = str(input("Digite o nome do quarto aluno: "))
+escolhido = random.choice([n1, n2, n3, n4]) #escolhe um nome aleatório dentro da lista []
+print('O aluno escolhido foi {}'.format(escolhido))
+
