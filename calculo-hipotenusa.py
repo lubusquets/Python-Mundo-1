a8bf71ecc6466d187fb17cc5ea428f3c28bba92e
Luciana Busquets <lubusquets@hotmail.com>
2026-06-05 22:28:14 -0300

    observações

--- calculo-hipotenusa.py
+++ calculo-hipotenusa.py
@@ -8,8 +8,8 @@ print('A hipotenusa é igual a {:.2f}'.format(h))
 co = float(input("Digite o valor do cateto oposto: "))
 ca = float(input("Digite o valor do cateto adjacente: "))
 h = (co**2 + ca**2)**1/2
-print('A hipotenusa é igual a {:.2f}'.format(h))
-''' 
+print('A hipotenusa é igual a {:.2f}'.format(h))'''
+ 
 print(40*'-')
 #cálculo de seno, cosseno e tangente
 
@@ -22,5 +22,13 @@ print('O seno de {} é {:.2f}'.format(angulo, seno))
 print('O cosseno de {} é {:.2f}'.format(angulo, cosseno))
 print('A tangente de {} é {:.2f}'.format(angulo, tangente))
 
-
+'''ou
+import math
+angulo = float(input("Digite o valor do ângulo: "))
+seno = math.sin(math.radians(angulo))   
+cosseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
+print('O angulo {} tem o seno {:.2f}'.format(angulo, seno))
+print('O angulo {} tem o cosseno {:.2f}'.format(angulo, cosseno))
+print('O angulo {} tem a tangente {:.2f}'.format(angulo, tangente))'''
 
--- reproduzir-mp3.py
+++ reproduzir-mp3.py
@@ -1,5 +1,1 @@
 #abrir e reproduzir um arquivo mp3
-import pygame
-pygame.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
