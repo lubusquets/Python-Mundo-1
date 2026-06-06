ccc7db911078a868e0dff812bca032bb5e1c14de
Luciana Busquets <lubusquets@hotmail.com>
2026-06-05 22:51:10 -0300

    simplificando o código

--- sorteio.py
+++ sorteio.py
@@ -24,3 +24,13 @@ print('O segundo aluno escolhido foi {}'.format(escolhido[1]))
 print('O terceiro aluno escolhido foi {}'.format(escolhido[2]))
 print('O quarto aluno escolhido foi {}'.format(escolhido[3]))
 
+#ou mais simples ainda
+import random
+n1 = str(input("Digite o nome do primeiro aluno: "))
+n2 = str(input("Digite o nome do segundo aluno: "))
+n3 = str(input("Digite o nome do terceiro aluno: "))
+n4 = str(input("Digite o nome do quarto aluno: "))
+lista = [n1,n2,n3,n4]
+random.shuffle(lista) #organiza a lista de forma aleatória
+print('A ordem de apresentação será ')
+print(lista)
