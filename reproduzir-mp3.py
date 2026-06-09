532dae374dd12f97191cc8db3ea890c9b09f47bf
Luciana Busquets <lubusquets@hotmail.com>
2026-06-08 22:27:30 -0300

    tocar mp3

--- reproduzir-mp3.py
+++ reproduzir-mp3.py
@@ -1,1 +1,11 @@
 #abrir e reproduzir um arquivo mp3
+import pygame
+pygame.init()
+pygame.mixer.music.load('C:\Users\Luciana\Documents\Curso em vídeo\Python\Python-Mundo-1\alive.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+print('Audio iniciado com sucesso!')
+while pygame.mixer.music.get_busy():
+    time.sleep(1)
+
+    
