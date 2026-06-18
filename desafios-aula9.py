a4e7c1da0de1f86ff595c67f4f27b815742320e2
Luciana Busquets <lubusquets@hotmail.com>
2026-06-17 21:18:58 -0300

    Enunciado dos desafios

--- desafios-aula9.py
+++ desafios-aula9.py
@@ -1,4 +1,4 @@
-'''Crie um programa que leia o nome completo de uma pessoa e mostre:
+'''Desafio 22 - Crie um programa que leia o nome completo de uma pessoa e mostre:
 - o nome com todas as letras maiúsculas 
 - o nome com todas as letras minúsculas
 - quantas letras ao todo (sem considerar espaços)
@@ -10,3 +10,22 @@ print('Seu nome em minúsculas é: {}'.format(nome.lower()))
 print('Seu nome tem {} letras'.format(len(nome.strip())))
 dividido = nome.split()
 print('Seu primeiro nome tem {} letras'.format(len(dividido[0])))
+print(50*'-')
+
+'''Desafio 23 - Crie um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.'''
+num = input('Digite um número entre 0 à 99999: ')
+print('Unidade: {}'.format(num[3]))
+
+print(50*'-')
+'''Desafio 24 - Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".'''
+
+print(50*'-')
+'''Desafio 25 - Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.'''
+
+print(50*'-')
+'''Desafio 26 - Crie um programa que leia uma frase e diga quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+
+print(50*'-')
+'''Desafio 27 - Crie um programa que leia o nome completo de uma pessoa e mostre:   - o primeiro nome   - o último nome'''  
+
+
