299abe6c4d2fbfbe6d7daa0d851d3e9e2a3d8ea3
Luciana Busquets <lubusquets@hotmail.com>
2026-06-18 17:26:52 -0300

    Obs desafio 22

--- desafios-aula9.py
+++ desafios-aula9.py
@@ -4,12 +4,14 @@
 - quantas letras ao todo (sem considerar espaços)
 - quantas letras tem o primeiro nome'''
 
-nome = input('Digite seu nome completo: ')
+nome = str(input('Digite seu nome completo: ')).strip()
 print('Seu nome em maiúsculas é: {}'.format(nome.upper()))
 print('Seu nome em minúsculas é: {}'.format(nome.lower()))  
-print('Seu nome tem {} letras'.format(len(nome.strip())))
+print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
+#print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
 dividido = nome.split()
-print('Seu primeiro nome tem {} letras'.format(len(dividido[0])))
+print('Seu primeiro nome é {} e tem {} letras'.format(dividido[0], len(dividido[0])))
+
 print(50*'-')
 
 '''Desafio 23 - Crie um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.'''
