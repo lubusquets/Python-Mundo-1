d7e1272390eac0e715163520be5b99571b1b6657
Luciana Busquets <lubusquets@hotmail.com>
2026-06-20 15:07:52 -0300

    contar letras da frase

--- desafio-24.py
+++ desafio-24.py
@@ -6,9 +6,17 @@ print('SANTO' in cidade.upper()) #prefiro este código que encontra a palavra em
 
 print(50*'-')
 '''Desafio 25 - Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.'''
+nome = str(input('Digite seu nome completo: ').strip())
+print('SILVA' in nome.upper())
+# print('silva' in nome.lower()) 
 
 print(50*'-')
 '''Desafio 26 - Crie um programa que leia uma frase e diga quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+
+frase = str(input('Digite uma frase: ')).strip()
+print('A letra "A" aparece {} vezes na frase.'.format(frase.upper().count('A')))
+print('A letra "A" aparece pela primeira vez na posição {}.'.format(frase.upper().find('A') + 1))
+print('A letra "A" aparece pela última vez na posição {}.'.format(frase.upper().rfind('A') + 1))
 
 print(50*'-')
 '''Desafio 27 - Crie um programa que leia o nome completo de uma pessoa e mostre:   - o primeiro nome   - o último nome'''  
