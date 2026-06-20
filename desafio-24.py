3a8ae7529a3bd0ed04202e9172f52178438ab558
Luciana Busquets <lubusquets@hotmail.com>
2026-06-20 14:59:59 -0300

    Desafio encontar nome

---
+++ desafio-24.py
@@ -0,0 +1,14 @@
+'''Desafio 24 - Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".'''
+
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
+#print(cidade[:5].upper() == 'SANTO')
+print('SANTO' in cidade.upper()) #prefiro este código que encontra a palavra em qualquer posição do nome da cidade, e não apenas no início.
+
+print(50*'-')
+'''Desafio 25 - Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.'''
+
+print(50*'-')
+'''Desafio 26 - Crie um programa que leia uma frase e diga quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+
+print(50*'-')
+'''Desafio 27 - Crie um programa que leia o nome completo de uma pessoa e mostre:   - o primeiro nome   - o último nome'''  
--- desafios-aula9.py
+++ desafios-aula9.py
@@ -25,17 +25,3 @@ print('Unidade: {}'.format(u))
 print('Dezena: {}'.format(d))
 print('Centena: {}'.format(c))
 print('Milhar: {}'.format(m))
-
-print(50*'-')
-'''Desafio 24 - Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".'''
-
-print(50*'-')
-'''Desafio 25 - Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.'''
-
-print(50*'-')
-'''Desafio 26 - Crie um programa que leia uma frase e diga quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
-
-print(50*'-')
-'''Desafio 27 - Crie um programa que leia o nome completo de uma pessoa e mostre:   - o primeiro nome   - o último nome'''  
-
-
